16ad40dc807a1895dc4ff44eb7583bb3ed600b3e
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2025-09-04 08:55:48 +0100

    Added a new feuture to the UI

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,13 @@
 const ScreenDisplay = () => {
     return (
-        <div className="screen">399,981</div>
+        <div className="screen">
+            <div className="previous-operand">
+                300,000 + 99,981
+            </div>
+            <div className="current-operand">
+                399,981
+            </div>
+        </div>
     );
 }
  
